668c5fa10eba116349987afe80e0a064767b4ae6
reedmohornSLC <reedmohornnc@gmail.com>
2017-04-11 11:31:56 -0600

    web server first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+var middleware= require('./middleware.js');
+
+//this will apply to every route hit and page requested, unless specified
+//within route//middleware should be expressed up top
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+//middleware.requireAuthentication makes middleware specifit
+app.get('/about',middleware.requireAuthentication, function(req,res){
+  res.send('About me, the taxpayer');
+});
+//this will be default root if no root / is defined, all files in public can now be seen
+app.use(express.static(__dirname+ '/public'));
+//using variable to go to this port
+var meeganport = 3500;
+
+app.listen(meeganport,function(){
+  console.log('Express Server Started on Port: '+ meeganport+'!')
+});
